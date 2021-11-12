2bfc8d6a8848f91258619eae93d4a76195ba562b
Nathan Lam <nathanlam@Nathans-MacBook-Pro.local>
2021-11-11 22:12:09 -0500

    read and write

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"./devices"
-
 	"github.com/go-chi/chi"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	// Setup DB Connection
+	connectionString := "host=0.0.0.0 port=5432 user=user password=password dbname=postgres sslmode=disable"
+
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS devices (name VARCHAR);`)
+	if err != nil {
+		panic(err)
+	}
+
+	// Setup API Routes
 	my_devices := devices.New()
 	log.Println("Starting up on http://localhost:8080")
 
@@ -38,4 +55,40 @@ func main() {
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func write(db sql.DB) {
+	insertStatement := `
+	INSERT INTO Devices (Name)
+	VALUES ('hehehhe');
+	`
+
+	_, err := db.Exec(insertStatement)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Added Device to Table")
+
+}
+
+func read(db sql.DB) {
+	selectQuery := "SELECT Name FROM Devices;"
+
+	rows, err := db.Query(selectQuery)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var Name string
+
+		err = rows.Scan(&Name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(Name)
+	}
+
 }
